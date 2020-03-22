08bcaaee18639e839411144ec3586cfb3bdc57bf
Vadzim Piatkevich <vadzim.piatkevich@gmail.com>
2020-03-23 00:08:10 +0300

    chore(portal): implement sidenav
    This commit implements an unresponsive sidebar with navigation links.

--- ruby/apps/portal/app/helpers/application_helper.rb
+++ ruby/apps/portal/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sidenav_link(path, name)
+    html_class = 'active' if current_page?(path)
+
+    content_tag(:li, class: html_class) do
+      link_to(name, path)
+    end
+  end
 end
