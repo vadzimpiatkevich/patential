6b60fde16ed4e6a8832f83e664e99fab67ad7dbc
Vadzim Piatkevich <vadzim.piatkevich@gmail.com>
2020-03-28 21:44:47 +0300

    chore(infra): change top-level dir names
    This commit changes the names of top-level dirs so that they're not conflicting with language binaries.

---
+++ golang/svc/patent/internal/service/service_test.go
@@ -0,0 +1,105 @@
+package service
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/google/go-cmp/cmp"
+	"github.com/patential/golang/pkg/testutil"
+	"github.com/patential/golang/svc/patent/internal/store"
+	pb "github.com/patential/golang/svc/patent/proto/service"
+	lgtest "github.com/sirupsen/logrus/hooks/test"
+)
+
+var logger, _ = lgtest.NewNullLogger()
+
+const testDBSchemaPath = "./testdata/schema.sql"
+
+func TestService_ListPatents(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	db, err := testutil.NewSqliteClient(ctx, testDBSchemaPath)
+	if err != nil {
+		t.Fatalf("Failed to prepare test DB: %v", err)
+	}
+	defer db.Close()
+
+	storeClient := store.NewClient(logger, db)
+
+	// moment is a "frozen" time for the current test context.
+	moment := time.Now()
+
+	// patents is the list of patent instances to insert.
+	patents := []store.Patent{
+		store.Patent{
+			ApplicationNumber: "application-number",
+			ApplicationKind:   "application-kind",
+			GrantDate:         moment.Add(time.Duration(1)),
+		},
+		store.Patent{
+			ApplicationNumber: "application-number-2",
+			ApplicationKind:   "application-kind-2",
+			GrantDate:         moment,
+		},
+	}
+
+	for _, p := range patents {
+		_, err := storeClient.InsertPatent(ctx, p)
+		if err != nil {
+			t.Fatalf("Failed to insert patent: %v", err)
+		}
+	}
+
+	service := New(logger, storeClient)
+
+	t.Run("with pagination params", func(t *testing.T) {
+		const (
+			limit  = 1
+			offset = 1
+		)
+
+		resp, err := service.ListPatents(
+			ctx,
+			&pb.ListPatentsRequest{
+				Pagination: &pb.Pagination{Limit: limit, Offset: offset},
+			},
+		)
+		if err != nil {
+			t.Errorf("Unexpected error returned: %v", err)
+			return
+		}
+		if len(resp.Patents) != limit {
+			t.Errorf(
+				"Wrong size of patents list returned (got=%d, expected=%d)",
+				len(resp.Patents), limit,
+			)
+			return
+		}
+
+		for i, p := range resp.Patents {
+			gdt, err := ptypes.Timestamp(p.GrantDate)
+			if err != nil {
+				t.Fatalf(
+					"Failed to convert grant date (grantDate=%v)", p.GrantDate,
+				)
+			}
+
+			// Map patent instance from Proto to Store.
+			got := store.Patent{
+				ApplicationNumber: p.ApplicationNumber,
+				ApplicationKind:   p.ApplicationKind,
+				GrantDate:         gdt,
+			}
+			want := patents[i+offset]
+
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf(
+					"Unexpected patent instance returned at index %d (diff=%s)", i, diff,
+				)
+			}
+		}
+	})
+}
--- ruby/apps/portal/README.md
+++
@@ -1,8 +0,0 @@
-This Rails app depends on the following protobuf libraries:
-- go/svc/patent/proto/service/service.proto.
-
-To produce Ruby library from the proto file, run the following command from the project root:
-
-```shell
-grpc_tools_ruby_protoc --ruby_out=lib/proto/svc/patent/ --grpc_out=lib/proto/svc/patent/ --proto_path=../../../go/svc/patent/proto/service service.proto
-```
